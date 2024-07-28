fac96764646dc8e245f40b7ec1a86186bccc35e9
bacngv <bac17012004@gmail.com>
2024-07-28 09:18:19 +0700

    fix training

--- main.py
+++ main.py
@@ -13,22 +13,19 @@ import torch
 import torch.nn.functional as F
 from transformers import AdamW, get_linear_schedule_with_warmup
 from torch import nn
-from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler,
-                              TensorDataset)
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
 
 from seqeval.metrics import classification_report
 from model.xlmr_for_token_classification import XLMRForTokenClassification
 from utils.train_utils import add_xlmr_args, evaluate_model
 from utils.data_utils import NerProcessor, create_dataset, convert_examples_to_features
 
-from tqdm import tqdm_notebook as tqdm
-from tqdm import trange
+from tqdm import tqdm, trange
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 
 
 def main():
@@ -68,9 +65,9 @@ def main():
         train_examples = processor.get_train_examples(args.data_dir)
         num_train_optimization_steps = int(
             len(train_examples) / args.train_batch_size / args.gradient_accumulation_steps) * args.num_train_epochs
-    
+
     # preparing model configs
-    hidden_size = 768 if 'base' in args.pretrained_path else 1024 # TODO: move this inside model.__init__
+    hidden_size = 768 if 'base' in args.pretrained_path else 1024  # TODO: move this inside model.__init__
 
     device = 'cuda' if (torch.cuda.is_available() and not args.no_cuda) else 'cpu'
 
@@ -82,7 +79,6 @@ def main():
     model.to(device)
     no_decay = ['bias', 'final_layer_norm.weight']
 
-    
     params = list(model.named_parameters())
 
     optimizer_grouped_parameters = [
@@ -104,7 +100,6 @@ def main():
         for n, p in model.named_parameters():
             if 'xlmr' in n and p.requires_grad:
                 p.requires_grad = False
-
 
     if args.fp16:
         try:
@@ -131,7 +126,7 @@ def main():
         train_data = create_dataset(train_features)
 
         train_sampler = RandomSampler(train_data)
-    
+
         train_dataloader = DataLoader(
             train_data, sampler=train_sampler, batch_size=args.train_batch_size)
 
@@ -141,24 +136,24 @@ def main():
             val_examples, label_list, args.max_seq_length, model.encode_word)
 
         val_data = create_dataset(val_features)
-        
+
         best_val_f1 = 0.0
 
-        for _ in tqdm(range(args.num_train_epochs), desc="Epoch"):
+        for epoch in trange(int(args.num_train_epochs), desc="Epoch"):
             tr_loss = 0
             nb_tr_examples, nb_tr_steps = 0, 0
 
-            tbar = tqdm(train_dataloader, desc="Iteration")
-            
+            tbar = tqdm(train_dataloader, desc="Iteration", total=len(train_dataloader))
+
             model.train()
             for step, batch in enumerate(tbar):
                 batch = tuple(t.to(device) for t in batch)
-                input_ids, label_ids, l_mask, valid_ids, = batch
+                input_ids, label_ids, l_mask, valid_ids = batch
                 loss = model(input_ids, label_ids, l_mask, valid_ids)
 
                 if args.gradient_accumulation_steps > 1:
                     loss = loss / args.gradient_accumulation_steps
-                
+
                 if args.fp16:
                     with amp.scale_loss(loss, optimizer) as scaled_loss:
                         scaled_loss.backward()
@@ -172,26 +167,25 @@ def main():
                 tr_loss += loss.item()
                 nb_tr_examples += input_ids.size(0)
                 nb_tr_steps += 1
-                tbar.set_description('Loss = %.4f' %(tr_loss / (step+1)))
+                tbar.set_postfix(loss=tr_loss / (step + 1))
 
                 if (step + 1) % args.gradient_accumulation_steps == 0:
                     optimizer.step()
                     scheduler.step()  # Update learning rate schedule
                     model.zero_grad()
                     global_step += 1
-            
+
             logger.info("\nTesting on validation set...")
             f1, report = evaluate_model(model, val_data, label_list, args.eval_batch_size, device)
             if f1 > best_val_f1:
                 best_val_f1 = f1
-                logger.info("\nFound better f1=%.4f on validation set. Saving model\n" %(f1))
-                logger.info("%s\n" %(report))
-                
+                logger.info("\nFound better f1=%.4f on validation set. Saving model\n" % (f1))
+                logger.info("%s\n" % (report))
+
                 torch.save(model.state_dict(), open(os.path.join(args.output_dir, 'model.pt'), 'wb'))
-            
-            else :
+            else:
                 logger.info("\nNo better F1 score: {}\n".format(f1))
-    else: # load a saved model
+    else:  # load a saved model
         state_dict = torch.load(open(os.path.join(args.output_dir, 'model.pt'), 'rb'))
         model.load_state_dict(state_dict)
         logger.info("Loaded saved model")
@@ -207,15 +201,14 @@ def main():
             raise ValueError("eval on dev or test set only")
         eval_features = convert_examples_to_features(
             eval_examples, label_list, args.max_seq_length, model.encode_word)
-        
+
         logger.info("***** Running evaluation *****")
         logger.info("  Num examples = %d", len(eval_examples))
         logger.info("  Batch size = %d", args.eval_batch_size)
-        
+
         eval_data = create_dataset(eval_features)
         f1_score, report = evaluate_model(model, eval_data, label_list, args.eval_batch_size, device)
 
-       
         logger.info("\n%s", report)
         output_eval_file = os.path.join(args.output_dir, "eval_results.txt")
         with open(output_eval_file, "w") as writer:
